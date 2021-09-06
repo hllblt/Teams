80df86eadb5fb54681a290350dae06eef0b6a9ad
Halil <halill.bulutt.1@gmail.com>
2021-09-06 21:29:11 +0200

    completed photo and info assignment

--- app.js
+++ app.js
@@ -1,7 +1,155 @@
+const leicester = [
+  {
+    name: "Kasper Schmeichel",
+    age: 34,
+    position: "GK",
+    src: "https://img.a.transfermarkt.technology/portrait/header/16911-1431779130.jpg?lm=1",
+  },
+  {
+    name: "James Justin",
+    age: 23,
+    position: "RB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/413220-1600097627.jpg?lm=1",
+  },
+  {
+    name: "Çağlar Söyüncü",
+    age: 25,
+    position: "CB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/320141-1622118720.jpg?lm=1",
+  },
+  {
+    name: "Wesley Fofana",
+    age: 20,
+    position: "CB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/475411-1576665174.jpg?lm=1",
+  },
+  {
+    name: "Ryan Bertrand",
+    age: 32,
+    position: "LB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/40611-1518794490.jpg?lm=1",
+  },
+  {
+    name: "Youri Tielemans",
+    age: 24,
+    position: "CM",
+    src: "https://img.a.transfermarkt.technology/portrait/header/249565-1626861318.jpg?lm=1",
+  },
+  {
+    name: "Wilfred Ndidi",
+    age: 24,
+    position: "DMC",
+    src: "https://img.a.transfermarkt.technology/portrait/header/274839-1601316687.jpg?lm=1",
+  },
+  {
+    name: "Harvey Barnes",
+    age: 23,
+    position: "ML",
+    src: "https://img.a.transfermarkt.technology/portrait/header/398065-1543494289.jpg?lm=1",
+  },
+  {
+    name: "James Maddison",
+    age: 24,
+    position: "AMC",
+    src: "https://img.a.transfermarkt.technology/portrait/header/294057-1538664596.jpg?lm=1",
+  },
+  {
+    name: "Patson Daka",
+    age: 22,
+    position: "CF",
+    src: "https://img.a.transfermarkt.technology/portrait/header/365172-1601103855.png?lm=1",
+  },
+  {
+    name: "Jamie Vardy",
+    age: 34,
+    position: "CF",
+    src: "https://img.a.transfermarkt.technology/portrait/header/197838-1583156197.jpg?lm=1",
+  },
+];
+const manutd = [
+  {
+    name: "David de Gea",
+    age: 30,
+    position: "GK",
+    src: "https://img.a.transfermarkt.technology/portrait/header/59377-1454414410.jpg?lm=1",
+  },
+  {
+    name: "Aaron Was-Bissaka",
+    age: 23,
+    position: "RB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/477758-1617030255.jpg?lm=1",
+  },
+  {
+    name: "Raphael Varane",
+    age: 28,
+    position: "CB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/164770-1598301905.jpg?lm=1",
+  },
+  {
+    name: "Harry Maguire",
+    age: 28,
+    position: "CB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/177907-1520608280.jpg?lm=1",
+  },
+  {
+    name: "Luke Shaw",
+    age: 26,
+    position: "LB",
+    src: "https://img.a.transfermarkt.technology/portrait/header/183288-1469631475.jpg?lm=1",
+  },
+  {
+    name: "Scott McTominay",
+    age: 24,
+    position: "CM",
+    src: "https://img.a.transfermarkt.technology/portrait/header/315969-1505839630.jpg?lm=1",
+  },
+  {
+    name: "Paul Pogba",
+    age: 28,
+    position: "CM",
+    src: "https://img.a.transfermarkt.technology/portrait/header/122153-1582114937.jpg?lm=1",
+  },
+  {
+    name: "Marcus Rashford",
+    age: 23,
+    position: "LW",
+    src: "https://img.a.transfermarkt.technology/portrait/header/258923-1565603308.png?lm=1",
+  },
+  {
+    name: "Jadon Sancho",
+    age: 21,
+    position: "RW",
+    src: "https://img.a.transfermarkt.technology/portrait/header/401173-1623778009.jpg?lm=1",
+  },
+  {
+    name: "Bruno Fernandes",
+    age: 26,
+    position: "AMC",
+    src: "https://img.a.transfermarkt.technology/portrait/header/240306-1580389882.jpg?lm=1",
+  },
+  { name: "Cristiano Ronaldo", age: 36, position: "CF", src: "https://img.a.transfermarkt.technology/portrait/header/8198-1626161872.jpg?lm=1" },
+];
+// const mancity = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const chelsea = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const arsenal = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const barcelona = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const realmadrid = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const atleticomadrid = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const villarreal = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+// const realsociedad = [{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},{name:"", age: , position:"", src: ""},];
+
 let openButton = document.getElementById("openButton");
 let closeButton = document.getElementById("closeButton");
 let leftSection = document.getElementById("leftSection");
 let rightSection = document.getElementById("rightSection");
+
+///////////PLAYER IMAGES////////////
+let playerImgRight = document.querySelectorAll(".playerImgRight");
+let playerImgLeft = document.querySelectorAll(".playerImgLeft");
+
+/////////PLAYER INFORMATIONS//////////
+let playerTextRight = document.querySelectorAll(".playerTextRight");
+let playerTextLeft = document.querySelectorAll(".playerTextLeft");
 
 openButton.addEventListener("click", () => {
   leftSection.style.width = "40%";
@@ -9,7 +157,31 @@ openButton.addEventListener("click", () => {
   rightSection.style.opacity = 1;
 });
 closeButton.addEventListener("click", () => {
-      leftSection.style.width = "80%";
-      rightSection.style.width = "0%";
-      rightSection.style.opacity = 0;
+  leftSection.style.width = "80%";
+  rightSection.style.width = "0%";
+  rightSection.style.opacity = 0;
 });
+
+const assignLeftTeam = (team, teamName) => {
+  for(let i = 0;i<team.length;i++){
+    playerImgLeft[i].src = team[i].src;
+    playerTextLeft[i].innerHTML = `Name: ${team[i].name}<br>Age: ${team[i].age}<br>Position: ${team[i].position}`;
+  }
+  document.getElementById("firstTeam").innerHTML = teamName;
+};
+const assignRightTeam = (team, teamName) => {
+  for(let i = 0;i<team.length;i++){
+    playerImgRight[i].src = team[i].src;
+    playerTextRight[i].innerHTML = `Name: ${team[i].name}<br>Age: ${team[i].age}<br>Position: ${team[i].position}`;
+  }
+  document.getElementById("secondTeam").innerHTML = teamName;
+};
+
+const teamSelect = (value) => {
+  if (value == "manutd") {
+    assignRightTeam(manutd, "Manchester United");
+  }
+  else if (value == "leicester") {
+    assignLeftTeam(leicester, "Leicester City");
+  }
+};
